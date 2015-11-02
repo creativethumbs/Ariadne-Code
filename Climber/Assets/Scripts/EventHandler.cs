7c31a6ce09f01003e3435202dfc0f44ee056f194
creativethumbs <ticha.seth@gmail.com>
2015-11-02 01:56:50 -0500

    decided to include everything just in case

--- Climber/Assets/Scripts/EventHandler.cs
+++ Climber/Assets/Scripts/EventHandler.cs
@@ -4,26 +4,34 @@ using System.Collections;
 public class EventHandler : MonoBehaviour {
 	private GameObject SpawnPoint;
 	private GameObject Player; 
+	public string nextLevel; 
 	public GameObject PlayerPrefab; 
 	private bool seen; 
 
 	// Use this for initialization
 	void Start() {
 		SpawnPoint = GameObject.FindWithTag ("SpawnPoint"); 
-		SpawnPoint.GetComponent<Renderer> ().enabled = false; 
+
+		if(SpawnPoint != null) 
+			SpawnPoint.GetComponent<Renderer> ().enabled = false; 
 
 		Player = GameObject.FindWithTag ("Player"); 
-		SpawnPoint.GetComponent<Transform>().localScale = Player.GetComponent<Transform>().localScale;
-		SpawnPoint.GetComponent<Transform>().position = Player.GetComponent<Transform>().position;
+		if (Player != null) {
+			SpawnPoint.GetComponent<Transform> ().localScale = Player.GetComponent<Transform> ().localScale;
+			SpawnPoint.GetComponent<Transform> ().position = Player.GetComponent<Transform> ().position;
+		}
 
 		seen = false;
 	}
 	
 	// Update is called once per frame
 	void Update () {
+		Player = GameObject.FindWithTag ("Player"); 
+
 		if (Player != null) {
 			if(Player.GetComponent<Transform>().lossyScale.x == 0) {
 				print ("player shrunk"); 
+				Application.LoadLevel(nextLevel); 
 			}
 
 			if (Player.GetComponent<Renderer> ().isVisible) {
@@ -32,16 +40,17 @@ public class EventHandler : MonoBehaviour {
 		
 			else if (seen) {
 				Destroy (Player); 
-				Player = null; 
-				SpawnPoint.GetComponent<Renderer> ().enabled = true; 
+				//Player = null; 
+				if(SpawnPoint != null) 
+					SpawnPoint.GetComponent<Renderer> ().enabled = true; 
 			}
 		}
 
 		// only works when player is not on screen
-		else if (Input.GetKeyDown (KeyCode.Return)) {
+		else if (SpawnPoint != null && Input.GetKeyDown (KeyCode.Return)) {
 			print ("return key is held down");
 			Instantiate (PlayerPrefab, new Vector2(SpawnPoint.transform.position.x,SpawnPoint.transform.position.y), Quaternion.identity);
-			Player = Player = GameObject.FindWithTag ("Player"); 
+			//Player = GameObject.FindWithTag ("Player"); 
 
 			SpawnPoint.GetComponent<Renderer>().enabled = false; 
 		}
